ed684bd1781e7f5167b0fb7f21047359f7c99d1c
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2015-11-27 10:11:13 -0500

    fix indentation and variable shadowing

--- markov.py
+++ markov.py
@@ -1,52 +1,55 @@
 import random
 import string
-def adjust(l):
-	a = 0
-	for (i,n) in enumerate(l):
-		a += n
-		l[i] = a
 
-def weighted_pick(l):
-	n = random.random()
-	i = 0
-	while i < 25:
-		if n < l[i]:
-			return i
-		i += 1
-	return i
 
-l = [[0]*26 for _ in range(26)]
- 
+def adjust(adj):
+    a = 0
+    for (i, n) in enumerate(adj):
+        a += n
+        adj[i] = a
+
+
+def weighted_pick(wtp):
+    n = random.random()
+    i = 0
+    while i < 25:
+        if n < wtp[i]:
+            return i
+        i += 1
+    return i
+
+
+l = [[0] * 26 for _ in range(26)]
+
 f = open("corpus.txt").read()
 words = f.split()
 
 for word in words:
-	for i in range(len(word)-1):
-		l[ord(word[i])-ord('a')][ord(word[i+1])-ord('a')] += 1
+    for i in range(len(word) - 1):
+        l[ord(word[i]) - ord('a')][ord(word[i + 1]) - ord('a')] += 1
 
 total = sum(map(sum, l))
 
-
 start = []
 for x in l:
-	start.append(sum(x) / total)
+    start.append(sum(x) / total)
 
 for y in range(len(l)):
-	subtotal = sum(l[y])
-	for x in range(len(l[y])):
-		l[y][x] /= subtotal
+    subtotal = sum(l[y])
+    for x in range(len(l[y])):
+        l[y][x] /= subtotal
 
 adjust(start)
 for y in l:
-	adjust(y)
+    adjust(y)
 
 for _ in range(20):
-	s = ""
-	old = weighted_pick(start)
-	s += string.ascii_lowercase[old]
+    s = ""
+    old = weighted_pick(start)
+    s += string.ascii_lowercase[old]
 
-	for _ in range(random.randint(2, 6)):
-		new = weighted_pick(l[old])
-		s += string.ascii_lowercase[new]
-		old = new
-	print(s)
+    for _ in range(random.randint(2, 6)):
+        new = weighted_pick(l[old])
+        s += string.ascii_lowercase[new]
+        old = new
+    print(s)
